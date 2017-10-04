74a260ef10bb67c17e4afd1d831ad809a1938997
David Grant <mrdavidgrant@outlook.com>
2017-10-04 12:46:26 -0400

    notification messages working

--- server.js
+++ server.js
@@ -2,25 +2,60 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
+const uuid4 = require ('uuid/v4')
 
 // Set the port to 3001
 const PORT = 3001;
 
+
 // Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
-  .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+// Make the express server serve static assets (html, javascript, css) from the /public folder
+.use(express.static('public'))
+.listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
+let messageBuffer = []
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-
+wss.on('connection', (client) => {
+  // console.log('Client connected, sending buffer');
+  // client.send(JSON.stringify(messageBuffer))
+  newConnection(client)
+  
+  client.onmessage = (event) => {
+    let msg = JSON.parse(event.data)
+    msg.uuid = uuid4()
+    switch(msg.type) {
+      case "postMessage":
+        msg.type = 'incomingMessage'
+        break
+      case "postNotification":
+        msg.type = 'incomingNotification'
+        break
+      default:
+        console.log(msg.type)
+    }
+    if (messageBuffer.length <= 10) {
+      messageBuffer.push(msg)
+    } else {
+      messageBuffer.shift().push(msg)
+    }
+    wss.clients.forEach(function each(client) {
+      client.send(JSON.stringify(msg))
+    })
+  }
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  client.on('close', () => console.log('Client disconnected'));
 });
+
+
+function newConnection (client) {
+  console.log("Client connected")
+  if (messageBuffer.length){
+    client.send(JSON.stringify(messageBuffer))
+  }
+}
